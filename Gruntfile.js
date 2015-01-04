947db55787323b8f589c8b43400db751626eadfb
Joe Teo <revelate@live.com>
2015-01-03 17:50:57 -0800

    add grunt modules and bower

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,15 +5,85 @@ module.exports = function(grunt) {
     'app/**/**/**.js'
   ];
 
+  grunt.loadNpmTasks('grunt-browserify');
   grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-jscs');
 
   grunt.initConfig({
+    browserify: {
+      dev: {
+        src: ['app/public/**/*.js'],
+        dest: 'build/bundle.js',
+        options: {
+          transform: ['debowerify']
+        }
+      }
+      // test: {
+      //   src: ['test/client/**/*test.js'],
+      //   dest:'test/test_bundle.js',
+      //   options:{
+      //     transform: ['debowerify']
+      //   }
+      // }
+    },
     jshint: {
       all: srcFiles,
       options: {
         jshintrc: true
       }
+    },
+    copy:{
+      app: {
+        cwd: 'app/',
+        src: ['*.html'],
+        expand: true,
+        dest: 'build/'
+      }
+    },
+    clean: {
+      dev: {
+        src: ['build/**/*.js']
+      }
+      // css: {
+      //   src: ['build/css/']
+      // }
+    },
+    uglify: {
+      my_target: {
+        options: {
+          compress: true
+        },
+        files: {
+          './build/bundle.min.js': ['./build/bundle.js']
+        }
+      }
+    },
+    watch: {
+      script: {
+        files: [
+          'app/**/**.js',
+          'app/**.js'
+        ],
+        tasks: ['build:js'],
+        options: {
+          livereload: true
+        }
+      }
+      // css: {
+      //   files: [
+      //     'app/assets/stylesheets/**/*.scss',
+      //     'app/assets/stylesheets/**/*.sass',
+      //     'app/**/*.html'
+      //   ],
+      //   tasks: ['build:css'],
+      //   options: {
+      //     livereload: true
+      //   }
+      // }
     },
     jscs: {
       src: srcFiles,
@@ -23,6 +93,8 @@ module.exports = function(grunt) {
     }
   });
 
+  grunt.registerTask('build:dev', ['clean:dev', 'copy:app', 'browserify:dev']);
+  grunt.registerTask('build', ['build:dev']);
   grunt.registerTask('test', ['jshint', 'jscs']);
   grunt.registerTask('default', ['test']);
 };
